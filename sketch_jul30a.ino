470267523d8260105cf5487df770b2db7aa62664
jeonghoon96 <73223953+jeonghoon96@users.noreply.github.com>
2020-11-04 16:29:01 +0900

    Update sketch_jul30a.ino
    code confirmation

--- sketch_jul30a.ino
+++ sketch_jul30a.ino
@@ -1,29 +1,27 @@
 #include <DHT11.h>
   
-extern volatile unsigned long timer0_millis;
+extern volatile unsigned long timer0_millis; // 시간 측정용 변수
 
-int pin = 10;
-int some = 0;
-DHT11 dht11(pin);
-int start_time;
-const int gasPin = A0 ;
-float temp_prev;
-float humi_prev;
-int inputPin = 2;     // 센서 신호핀
-int val = 0;          // 센서 신호의 판별을 위한 변수
-int speaker = 9;
-int sound = 0;
+const int pin = 10;      // 온습도센서
+DHT11 dht11(pin);        // DHT11(온습도센서) 핀 할당
+const int gasPin = A0;   // 가스센서
+const int inputPin = 2;  // 인체감지센서
+const int speaker = 9;   // 부저스피커
+
+float temp_prev;  // 온도측정값
+float humi_prev;  // 습도측정값
+
+int val = 0;      // 인체감지센서 측정값(HIGH/LOW)
+int sound = 0;    // 필요없는 변수?    
+int some = 0;     // 인체감지센서 cool down 변수
+int start_time;   // 인체감지 측정 시각
+
 void setup() {
     Serial.begin(9600);
-
-    //dht11.begin();
     pinMode(speaker,OUTPUT);
     pinMode(inputPin, INPUT);    // 센서 Input 설정
 
 }
-
-
-
 
 void loop() {
   delay(600);
@@ -33,34 +31,22 @@ void loop() {
  
   if((err=dht11.read(humi,temp))==0){
       temp_prev = temp;
-      humi_prev = humi; 
-    
-       
+      humi_prev = humi;        
   }
   
-  
-  val = digitalRead(inputPin);         // 센서 신호값을 읽어와서 val에 저장
-  
-  if (val == HIGH) {  
-      start_time = (int)millis();    // 센서 신호값이 HIGH면(인체 감지가 되면)    
-      some = 1;    // 시리얼 모니터 출력
-     
-     
-   } 
-   else {                             // 센서 신호값이 LOW면(인체감지가 없으면)
-       if((int)millis() - start_time > 5000){
-       some = 0;
-       
-       
-       
-       noTone(speaker);
-       //timer0_millis = 0;
-       
-       }
-   }
+  val = digitalRead(inputPin);         // 인체감지센서 신호값을 읽어와서 val에 저장
+  if (val == HIGH) {                   // 센서 신호값이 HIGH면(인체감지가 된 경우)
+      start_time = (int)millis();          
+      some = 1;    
+  } 
+  else {                                       // 센서 신호값이 LOW면(인체감지가 되지 않은 경우)
+      if((int)millis() - start_time > 5000){   // 인체감지가 되지 않는 시간이 5초 이상
+      some = 0;
+      noTone(speaker);    
+      }
+  }
    
-   if(some == 1 && sound == 0){
-   
+  if(some == 1 && sound == 0){         // 인체감지후 5초 이내 부저 동작
      tone(speaker,1200);
      delay(50);
      noTone(speaker);
@@ -75,23 +61,18 @@ void loop() {
      noTone(speaker);
      tone(speaker,1509);
      delay(50);
-     noTone(speaker);
-     
+     noTone(speaker);     
    }
    
-   
-   
-     //Serial.println();
-    Serial.println("start");
-    Serial.print(temp_prev);
-    Serial.print("k");
-    Serial.print(humi_prev);
-    Serial.print("k"); 
-    Serial.print(1000 + analogRead(gasPin));
-    Serial.print("k");
-    Serial.println(some);
+  Serial.println("start");                  // Serial start
+  Serial.print(temp_prev);
+  Serial.print("k");
+  Serial.print(humi_prev);
+  Serial.print("k"); 
+  Serial.print(1000 + analogRead(gasPin));
+  Serial.print("k");                        
+  Serial.println(some);                     // Serial end
   
- 
   delay(600);
   
 }
